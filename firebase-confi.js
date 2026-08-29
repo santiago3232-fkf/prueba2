d711566f8bcd583e2b4195fdb17fe02969361240
Naii <san.18gutierrez@gmail.com>
2026-08-29 10:20:06 -0500

    login parte 1
    primera parte del login, se creo la fire base el csss y html

---
+++ firebase-confi.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js"
+import { getFirestor } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js"
+
+export const firebaseConfig = { 
+    apiKey: "AIzaSyCOKOpU4TJn83TilY3_aXfWfibidnYMiaM",
+  authDomain: "paynex-23d0d.firebaseapp.com",
+  projectId: "paynex-23d0d",
+  storageBucket: "paynex-23d0d.firebasestorage.app",
+  messagingSenderId: "1000368632531",
+  appId: "1:1000368632531:web:a33ac9448375253d079a26",
+  measurementId: "G-SCX1NF6349"
+};
